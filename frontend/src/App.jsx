2aef136aa515d3a3ac0074feef91efc7dc7705fc
Aakash-Sondagar <aakashsondar@gmail.com>
2024-05-15 23:25:40 +0530

    routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,43 @@
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+const Layout = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 };
 
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,16 @@
+import X from "../components/svgs/X";
+
+const SignUp = () => {
+  return (
+    <div className="max-w-screen-xl">
+      <div className="left">
+        <X className=" lg:w-2/3 fill-white" />
+      </div>
+      <div className="right">
+        <h1>Sign Up</h1>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
