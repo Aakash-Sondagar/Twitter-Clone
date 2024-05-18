23e7d706257b94d37f6c81e80b501e6ac7a97994
Aakash-Sondagar <aakashsondar@gmail.com>
2024-05-18 23:52:01 +0530

    Changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 
+import { GoSun } from "react-icons/go";
+import { GoMoon } from "react-icons/go";
+
 import Posts from "../components/common/Posts";
 import CreatePost from "../components/common/CreatePost";
 
@@ -12,7 +15,8 @@ const Home = () => {
       <div className="flex w-full border-b border-gray-700">
         <div
           className={
-            "flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+            "flex justify-center flex-1 mt-2 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative" +
+            (feedType === "forYou" ? " font-bold" : "")
           }
           onClick={() => setFeedType("forYou")}
         >
@@ -22,13 +26,27 @@ const Home = () => {
           )}
         </div>
         <div
-          className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+          className={
+            "flex justify-center flex-1 mt-2 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative" +
+            (feedType === "following" ? " font-bold" : "")
+          }
           onClick={() => setFeedType("following")}
         >
           Following
           {feedType === "following" && (
-            <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
+            <div className="absolute bottom-0 w-14 h-1 rounded-full bg-primary"></div>
           )}
+        </div>
+        <div className="p-2 my-auto mt-2 mr-2">
+          <label className="swap swap-rotate">
+            <input
+              type="checkbox"
+              className="theme-controller"
+              value="synthwave"
+            />
+            <GoSun className="swap-off fill-current w-6 h-6" />
+            <GoMoon className="swap-on fill-current w-6 h-6" />
+          </label>
         </div>
       </div>
 
