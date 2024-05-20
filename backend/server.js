c0d971717feb116b48acc12cb059754cb4fe68f2
Aakash-Sondagar <aakashsondar@gmail.com>
2024-05-20 17:36:41 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
@@ -20,9 +21,13 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({
-    limit: "5mb"
-})); // to parse JSON bodies into JS objects
+const __dirname = path.resolve();
+
+app.use(
+  express.json({
+    limit: "5mb",
+  })
+); // to parse JSON bodies into JS objects
 app.use(express.urlencoded({ extended: true })); // to parse URL-encoded bodies
 app.use(cookieParser());
 
@@ -30,6 +35,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
